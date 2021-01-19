52944de2227c98aae7734b56d5025c25a9d38edd
Lau Llobet <lau.llobet@mypulse.ai>
2021-01-19 16:36:00 +0100

    🔴🔴🔴🔴 RED - prevent playing two times in the same position - to squash

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,21 @@
 import { expect } from 'chai';
- 
+ /*
+ 📌 ({} → nil) no code at all → code that employs nil
+    (nil → constant)
+    (constant → constant+) a simple constant to a more complex constant
+    (constant → scalar) replacing a constant with a variable or an argument
+    (statement → statements) adding more unconditional statements.
+    (unconditional → if) splitting the execution path
+    (scalar → array)
+    (array → container)
+    (statement → tail-recursion)
+    (if → while)
+    (statement → non-tail-recursion)
+    (expression → function) replacing an expression with a function or algorithm
+    (variable → assignment) replacing the value of a variable.
+    (case) adding a case (or else) to an existing switch or if
+*/
+
 describe('TicTacToe Should', () => {
     it('prevent playing out of the board', () => {
         let tictactoe = new TicTacToe()
@@ -17,6 +33,13 @@ describe('TicTacToe Should', () => {
         let tictactoe = new TicTacToe();
         expect(tictactoe.postAMove(1, 1, 'X'))
         .eql({ 'status': 'OK'});
+    })
+    it('prevent playing two times in the same position',() => {
+        let tictactoe = new TicTacToe();
+
+        tictactoe.postAMove(1,1,'X')
+        expect(tictactoe.postAMove(1, 1, 'X'))
+        .eql({ 'error': 'move on already taken place'});
     })
 });
 class TicTacToe {
